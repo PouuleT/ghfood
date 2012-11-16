8fb3c41366d12c89df2b1c955c938d16a32d77be
Paul Chobert <paul@chobert.fr>
2012-11-16 23:56:32 +0100

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghfood::Application.config.secret_token = 'bebdb20cc9b4ba00290fa982df1b3d7588e381629396cf563b8ae08e17434783711b869a97b13618e51ce055ca52ca2168abf9ac88810f977e792fc1e3c04e89'
